7f0a3c0a5a573f6702db0b439ff3bb7a62f17cac
joywins2 <joywins@daum.net>
2018-07-28 09:21:23 +0900

    Bean 의 Lifecycle 과 외부파일 참조 설정

--- app012/src/main/java/org/joy/beanLifeEx041/EnvXML/MainExt2.java
+++ app012/src/main/java/org/joy/beanLifeEx041/EnvXML/MainExt2.java
@@ -6,7 +6,7 @@ public class MainExt2 {
 	public static void main(String[] args){
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ExtConfig.class);
 		System.out.println("sucess!!");
-		ExternalFileEx ext = ctx.getBean("getExtConf" ,ExternalFileEx.class);
+		ExternalFileEx ext = ctx.getBean("ExtConfig" ,ExternalFileEx.class);
 		
 		System.out.println("envId : "+ext.getId());
 		System.out.println("envPwd : "+ext.getPwd());
