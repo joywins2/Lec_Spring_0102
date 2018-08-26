f00ba24dc1e78ad6ac703f3d36656c88a227c651
joywins2 <joywins@daum.net>
2018-08-26 19:10:02 +0900

    form의 GET/POST method와 form의 동일 action에 대한 Controller  메서드 오버로딩을 이용한 처리

---
+++ app014/src/main/java/org/joy/web/SampleController03.java
@@ -0,0 +1,79 @@
+package org.joy.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/sample03/*")
+public class SampleController03 {
+	
+	@RequestMapping("/inputGET")
+	public String inputGET(){
+		return "sample03/inputFormGET";
+	}
+	
+	@RequestMapping("/inputPOST")
+	public String inputPOST(){
+		return "sample03/inputFormPOST";
+	}
+	
+	
+	@RequestMapping(value="/person", method=RequestMethod.GET)
+	public String person(HttpServletRequest request, Model model){
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		
+		System.out.println("personGET name : "+name);
+		System.out.println("personGET id : "+id);
+		
+		model.addAttribute("name",name);
+		model.addAttribute("id", id);
+		
+		
+		return "sample03/personInfoGET";
+	}
+	
+//	@RequestMapping(value="/personPOST", method=RequestMethod.POST)
+//	public String person(HttpServletRequest request, Model model){
+//		
+//		String name = request.getParameter("name");
+//		String id = request.getParameter("id");
+//		
+//		System.out.println("name : "+name);
+//		System.out.println("id : "+id);
+//		
+//		model.addAttribute("name",name);
+//		model.addAttribute("id", id);
+//		
+//		return "sample03/personInfoPOST";
+//	}//goPerson()
+	
+	//...GET 방식의 person 메서드가 있으므로 매개변수 조정한 오버로딩 사용.
+	@RequestMapping(value="/person", method=RequestMethod.POST)
+	public ModelAndView person(HttpServletRequest request){
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		
+		System.out.println("personPOST name : "+name);
+		System.out.println("personPOST id : "+id);
+		
+		ModelAndView modelView = new ModelAndView();
+		
+		modelView.setViewName("sample03/personInfoPOST");
+		modelView.addObject("name",name);
+		modelView.addObject("id",id);
+		
+//		model.addAttribute("name",name);
+//		model.addAttribute("id", id);
+		
+		return modelView;
+	}//goPerson()
+	
+}
